97a1cfdd9144f1546d00caade5b87c3acf7a3dc6
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2026-04-07 14:05:24 +0700

    feat: collection with permit support

---
+++ src/interfaces/IDigitalCollectibles.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+
+import "./IERC4494.sol";
+
+interface IDigitalCollectibles is IERC721, IERC4494, IERC721Metadata {
+    /// @notice Function to approve by way of owner signature, for all Collectibles os owner
+    /// @param owner the address initiated the approval
+    /// @param spender the address to approve
+    /// @param deadline a timestamp expiry for the permit
+    /// @param signature a traditional or EIP-2098 signature
+    function permitForAll(address owner, address spender, uint256 deadline, bytes memory signature) external;
+
+    /// @notice Returns the nonce of an address - useful for creating permits
+    /// @param owner owner address to get the nonce of
+    /// @return the uint256 representation of the nonce
+    function nonceByAddress(address owner) external view returns (uint256);
+}
